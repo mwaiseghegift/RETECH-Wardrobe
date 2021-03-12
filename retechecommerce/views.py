8c9581c4446f40af29eeeb155785a9bfa6d081ac
Gateway47 <itsregalo047@gmail.com>
2021-03-12 14:07:55 +0300

    frontend

--- retechecommerce/views.py
+++ retechecommerce/views.py
@@ -13,3 +13,69 @@ def IndexView(request, *args, **kwargs):
         'new_products': new_products,
     }
     return render(request, 'index.html', context)
+
+def ContactView(request, *args, **kwargs):
+    context = {
+        
+    }
+    return render(request, 'contact.html', context)
+
+def BlogView(request, *args, **kwargs):
+    context = {
+        
+    }
+    return render(request, 'blog.html', context)
+
+def BlogDetailView(request, slug, *args, **kwargs):
+    context = {
+        
+    }
+    return render(request, 'blog-detail.html', context)
+
+def CartView(request, *args, **kwargs):
+    context = {
+        
+    }
+    return render(request, 'cart.html', context)
+
+def CheckOut(request, *args, **kwargs):
+    context = {
+        
+    }
+    return render(request, 'checkout.html', context)
+
+def CustomerReview(request, *args, **kwargs):
+    context = {
+        
+    }
+    return render(request, 'customer-review.html', context)
+
+def Shop(request, *args, **kwargs):
+    context = {
+        
+    }
+    return render(request, 'shop.html', context)
+
+def WishList(request, *args, **kwargs):
+    context = {
+        
+    }
+    return render(request, 'wishlist.html', context)
+
+def Team(request, *args, **kwargs):
+    context = {
+        
+    }
+    return render(request, 'team.html', context)
+
+def Portfolio(request, *args, **kwargs):
+    context = {
+        
+    }
+    return render(request, 'portfolio-card-box-2.html', context)
+
+def About(request, *args, **kwargs):
+    context = {
+        
+    }
+    return render(request, 'about.html', context)
