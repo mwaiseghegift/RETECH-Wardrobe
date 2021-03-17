7c922965208a9efbb078c918e51589617a904000
Gateway47 <itsregalo047@gmail.com>
2021-03-17 21:56:42 +0300

    made signals and they working perfectly.. its time to watch manifest..I call it a day

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Profile
 # Register your models here.
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user']
+
+admin.site.register(Profile, ProfileAdmin)
