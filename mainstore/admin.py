99e2030ccc8ce9b30985a809f2f2778e2631fd7e
Gateway47 <itsregalo047@gmail.com>
2021-03-23 12:30:58 +0300

    imporoved UI/UX

--- mainstore/admin.py
+++ mainstore/admin.py
@@ -14,7 +14,10 @@ admin.site.site_header = "RETECH Mall"
 admin.site.site_title = "RETECH Mall"
 
 # Register your models here.
-admin.site.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category']
+admin.site.register(Item, ItemAdmin)
+
 admin.site.register(Manufacture)
 admin.site.register(Upcoming_Product)
 
