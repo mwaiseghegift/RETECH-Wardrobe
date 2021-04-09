44987981955b69e3cba2e307437286328b46cbd2
Gateway47 <itsregalo047@gmail.com>
2021-04-09 22:36:50 +0300

    fixed bugs

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY =config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '45291b1fab33.ngrok.io','localhost']
+ALLOWED_HOSTS = ['127.0.0.1', '1ce8ea8f6c35.ngrok.io','localhost']
 
 
 # Application definition
